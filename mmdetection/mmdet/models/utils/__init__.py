b70bac1755dea8ec7179a5b25119fcc9dbb3b324
SirPok <s6030211031@phuket.psu.ac.th>
2021-02-15 03:23:38 +0700

    come back

--- mmdetection/mmdet/models/utils/__init__.py
+++ mmdetection/mmdet/models/utils/__init__.py
@@ -1,16 +1,12 @@
-from .builder import build_positional_encoding, build_transformer
-from .gaussian_target import gaussian_radius, gen_gaussian_target
-from .positional_encoding import (LearnedPositionalEncoding,
-                                  SinePositionalEncoding)
-from .res_layer import ResLayer, SimplifiedBasicBlock
-from .transformer import (FFN, DynamicConv, MultiheadAttention, Transformer,
-                          TransformerDecoder, TransformerDecoderLayer,
-                          TransformerEncoder, TransformerEncoderLayer)
+from .conv_module import ConvModule, build_conv_layer
+from .conv_ws import ConvWS2d, conv_ws_2d
+from .norm import build_norm_layer
+from .scale import Scale
+from .weight_init import (bias_init_with_prob, kaiming_init, normal_init,
+                          uniform_init, xavier_init)
 
 __all__ = [
-    'ResLayer', 'gaussian_radius', 'gen_gaussian_target', 'MultiheadAttention',
-    'FFN', 'TransformerEncoderLayer', 'TransformerEncoder',
-    'TransformerDecoderLayer', 'TransformerDecoder', 'Transformer',
-    'build_transformer', 'build_positional_encoding', 'SinePositionalEncoding',
-    'LearnedPositionalEncoding', 'DynamicConv', 'SimplifiedBasicBlock'
+    'conv_ws_2d', 'ConvWS2d', 'build_conv_layer', 'ConvModule',
+    'build_norm_layer', 'xavier_init', 'normal_init', 'uniform_init',
+    'kaiming_init', 'bias_init_with_prob', 'Scale'
 ]
